92b0bc992d03225757739d46fe78423cc343a42c
Moshe Kolodny <kolodny@github.com>
2025-07-01 11:45:28 -0400

    fix import path

--- src/dirname.ts
+++ src/dirname.ts
@@ -1,1 +1,4 @@
-export default import.meta.dirname;
+const dir = import.meta.dirname || new URL(import.meta.resolve('.')).pathname;
+const dirname = dir.endsWith('/') ? dir.slice(0, -1) : dir;
+
+export default dirname;
